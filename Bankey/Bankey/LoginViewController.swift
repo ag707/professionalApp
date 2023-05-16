d8aaa4fe47704838c7fec6240d20532500eb2bb6
ag707 <36295740+ag707@users.noreply.github.com>
2023-05-16 16:40:57 +0300

    Show onboarding only once

--- Bankey/Bankey/LoginViewController.swift
+++ Bankey/Bankey/LoginViewController.swift
@@ -7,6 +7,14 @@
 
 import UIKit
 
+protocol LogoutDelegate: AnyObject {
+  func didLogout()
+}
+
+protocol LoginViewControllerDelegate: AnyObject {
+  func didLogin()
+}
+
 class LoginViewController: UIViewController {
 
   let titleLabel = UILabel()
@@ -14,6 +22,8 @@ class LoginViewController: UIViewController {
   let loginView = LoginView()
   let signInButton = UIButton(type: .system)
   let errorMessageLabel = UILabel()
+
+  weak var delegate: LoginViewControllerDelegate?
 
   var username: String? {
     return loginView.usernameTextField.text
@@ -29,6 +39,12 @@ class LoginViewController: UIViewController {
     view.backgroundColor = .systemBackground
     style()
     layout()
+  }
+
+
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    signInButton.configuration?.showsActivityIndicator = false
   }
 
 
@@ -130,13 +146,14 @@ extension LoginViewController {
           return
         }
 
-        if username.isEmpty || password.isEmpty {
-          configureView(withMessage: "Username / Password cannot be blank")
-          return
-        }
+//        if username.isEmpty || password.isEmpty {
+//          configureView(withMessage: "Username / Password cannot be blank")
+//          return
+//        }
 
-        if username == "Kevin" && password == "Welcome" {
+        if username == "" && password == "" {
           signInButton.configuration?.showsActivityIndicator = true
+          delegate?.didLogin()
         } else {
           configureView(withMessage: "Incorrect username / password")
         }
